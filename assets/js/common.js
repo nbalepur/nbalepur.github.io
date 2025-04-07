cf3db9ead120843003e3ec25afacce4b637d348a
Nishant Balepur <55101514+nbalepur@users.noreply.github.com>
2025-04-07 19:30:59 -0400

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
   // add toggle functionality to abstract and bibtex buttons
   $('a.abstract').click(function() {
-    $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".thoughts.hidden.open").toggleClass('open');
   });
   $('a.tldr').click(function() {
-    $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
     $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
+    $(this).parent().parent().find(".thoughts.hidden.open").toggleClass('open');
   });
   $('a.bibtex').click(function() {
     $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
     $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".thoughts.hidden.open").toggleClass('open');
+  });
+  $('a.thoughts').click(function() {
+    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
+    $(this).parent().parent().find(".thoughts.hidden").toggleClass('open');
   });
   $('a').removeClass('waves-effect waves-light');
 
